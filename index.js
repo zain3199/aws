055331ecbdd5b5e8d031a8b5a17e75094a5d2034
zain3199 <111056205+zain3199@users.noreply.github.com>
2026-05-20 20:08:19 +0500

    zain

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = 3000;
 
-// GET API
+// Middleware (important for APIs)
+app.use(express.json());
+
+// =====================
+// MongoDB CONNECTION
+// =====================
+mongoose
+  .connect("mongodb+srv://USERNAME:PASSWORD@cluster0.xxxxx.mongodb.net/mydb")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
+
+// =====================
+// TEST ROUTE
+// =====================
 app.get("/", (req, res) => {
   res.send("Hello from Node.js API 🚀");
 });
 
-// Server start
-app.listen(PORT, () => {
+// =====================
+// START SERVER
+// =====================
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
